bc45083efe39ee2231c550fbaf49ec1b60526edd
Mayur <mayurkumbar589@gmail.com>
2025-06-29 12:11:13 +0530

    Product Service New Version updated

--- src/main/java/com/tcs/tcskart/bean/ProductImages.java
+++ src/main/java/com/tcs/tcskart/bean/ProductImages.java
@@ -1,34 +1,17 @@
 package com.tcs.tcskart.bean;
 
-import java.util.Base64;
+import jakarta.persistence.*;
+import lombok.*;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
-import jakarta.persistence.Basic;
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Embeddable
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductImages {
-    private String filename;
-    private String contentType;
 
-    @Lob
-    @Basic(fetch = FetchType.LAZY)
-    @Column(columnDefinition="LONGBLOB")
-    private byte[] data;
-    public String getBase64() {
-        return Base64.getEncoder().encodeToString(this.data);
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String url;
 }
-
